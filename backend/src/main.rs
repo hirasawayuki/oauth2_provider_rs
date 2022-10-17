f76eea6336aaa6462392053530900f248df7013c
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-10-17 21:56:32 +0900

    feature index page

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,26 @@
-use actix_web::{Responder, HttpServer, HttpResponse, get, App, web};
+use actix_web::{error, HttpServer, HttpResponse, App, web, Error};
+use tinytemplate::TinyTemplate;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!!")
-}
+async fn index(
+    template: web::Data<TinyTemplate<'_>>,
+) -> Result<HttpResponse, Error> {
+    let s = template.render("index.html", &serde_json::Value::Null)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Manual Hello World!!")
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let mut tt = TinyTemplate::new();
+        tt.add_template("index.html", INDEX).unwrap();
         App::new()
-            .service(hello)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(web::Data::new(tt))
+            .service(web::resource("/").route(web::get().to(index)))
     }).bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
+
+static INDEX: &str = include_str!("../templates/index.html");
